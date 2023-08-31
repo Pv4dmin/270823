f6f5ecd18c5a466409a44df7c9948bfc9c48e424
Rem <Pvadmin@mail.ru>
2023-08-31 18:27:49 +0800

    программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.

---
+++ work3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int InputNum(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int fact(int num)
+{
+    int f = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int number = InputNum("Введите число: ");
+int result = fact(number);
+Console.WriteLine($"факториал числа {number} равна {result}");
+
---
+++ work4_2/Program.cs
@@ -0,0 +1,68 @@
+﻿// int InputNum(string message)
+// {
+// Console.Write(message);
+// return int.Parse(Console.ReadLine()!);
+// }
+
+// int[] CreateArray(int size)
+// {
+// return new int[size];
+// }
+
+// void FillArray(int[] ints)
+// {
+// Random rnd = new Random();
+// for (int i = 0; i < ints.Length; i++)
+// ints[i] = rnd.Next(2);
+// }
+
+// string PrintArray(int[] ints)
+// {
+// string res = ""; // res = String.Empty;
+// for (int i = 0; i < ints.Length; i++)
+// res += $"{ints[i]} ";
+// return res;
+// }
+
+// int size = InputNum("Введите размер массива: ");
+// int[] myArray = CreateArray(size);
+// FillArray(myArray);
+// string answer = PrintArray(myArray);
+// System.Console.WriteLine(answer);
+
+// void Func(int num) // параметр
+// {
+// num = num / 2;
+// System.Console.WriteLine("num = " + num);
+// }
+
+
+// int a = 5;
+// System.Console.WriteLine(a);
+// Func(a); // аргумент
+// System.Console.WriteLine(a);
+
+int[] array1 = {1, 2, 3, 4, 5};
+// int[] array2 = array1;
+// Console.WriteLine(String.Join(" ", array1));
+// Console.WriteLine(String.Join(" ", array2));
+// System.Console.WriteLine("______________________");
+// array1[2] = 10;
+// Console.WriteLine(String.Join(" ", array1));
+// Console.WriteLine(String.Join(" ", array2));
+// System.Console.WriteLine("______________________");
+// array2[4] = 15;
+// Console.WriteLine(String.Join(" ", array1));
+// Console.WriteLine(String.Join(" ", array2));
+// int[] array2 = new int[array1.Length];
+// for (int i = 0; i < array1.Length; i++)
+// {
+// array2[i] = array1[i];
+// }
+// Console.WriteLine(String.Join(" ", array1));
+// Console.WriteLine(String.Join(" ", array2));
+// System.Console.WriteLine("______________________");
+// array1[2] = 10;
+// Console.WriteLine(String.Join(" ", array1));
+// Console.WriteLine(String.Join(" ", array2));
+// System.Console.WriteLine("______________________");
