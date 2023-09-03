77aaebd95e90b71cb3ba80210a3ca7ecf7bca17f
Your Name <you@example.com>
2023-09-03 13:26:38 +0800

    Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.

--- hwork3/Program.cs
+++ hwork3/Program.cs
@@ -8,10 +8,10 @@ int InputNum(string message)
 }
 
 int[] ArrayOfNumbers(int[] size)
-{
+{ 
     int[] array = new int[size];
     Console.Write("[");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i< size; i++)
     {
         int array[i] = new Random().Next(1);
         Console.Write(array[i]);
